b8da47bfe8a2aaf557d4f483b1eea007662a3dff
visaenator <visa.seppanen@gmail.com>
2016-05-16 20:26:23 +0300

    Code for the servo and button

---
+++ servo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int switchState = 0;
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600);
+  pinMode(12, INPUT);
+
+}
+
+void loop() {
+  switchState = digitalRead(12);
+  if(switchState != 0){
+     myServo.write(179);
+     delay(2000);
+  }
+  myServo.write(0);
+  delay(100);
+}
